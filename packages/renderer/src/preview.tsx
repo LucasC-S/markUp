91e5a405dc7ba60d183de2a84b198e1d68eb433e
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 <lucas.crawhallstein@ontariotechu.net>
2021-12-10 19:56:36 -0500

    added TeX support using remark-math

--- packages/renderer/src/preview.tsx
+++ packages/renderer/src/preview.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkGfm from 'remark-gfm'
-import remarkReact from 'remark-react'
+import remarkMath from 'remark-math'
 import RemarkCode from './remark-code'
-import { defaultSchema } from 'hast-util-sanitize'
+import remarkRehype from 'remark-rehype'
+import rehypeMathjax from 'rehype-mathjax'
+import rehypeReact from 'rehype-react'
+import { defaultSchema } from 'rehype-sanitize'
 import './preview.css'
 import 'github-markdown-css/github-markdown.css'
+import rehypeSanitize from 'rehype-sanitize'
+
 
 interface Props {
     doc: string
@@ -14,20 +19,31 @@ interface Props {
 
 const schema = {
     ...defaultSchema,
-    attributes: {
-        ...defaultSchema.attributes,
-        code: [...(defaultSchema.attributes?.code || []), 'className']
-    }
-}
+          attributes: {
+            ...defaultSchema.attributes,
+            code: [...(defaultSchema.attributes?.code || []), 'className'],
+            div: [
+              ...(defaultSchema.attributes.div || []),
+              ['className', 'math', 'math-display']
+            ],
+            span: [
+              ...(defaultSchema.attributes.span || []),
+              ['className', 'math', 'math-inline']
+            ]
+          }
+  }
 
 const Preview: React.FC<Props> = (props) => {
     const md = unified()
     .use(remarkParse)
     .use(remarkGfm)
-    .use(remarkReact, {
+    .use(remarkMath)
+    .use(remarkRehype)
+    .use(rehypeSanitize, schema)
+    .use(rehypeMathjax)
+    .use(rehypeReact, {
         createElement: React.createElement,
-        sanitize: schema,
-        remarkReactComponents: {
+        components: {
             code: RemarkCode
         }
     })
